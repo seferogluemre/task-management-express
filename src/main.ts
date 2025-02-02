5576cbf7d501b99eaf875c3401495456ebd36afe
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-02-03 02:36:31 +0300

    created user and task schemas

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.post("/users", (req, res) => {
+    res.send("Kullanıcı oluşturuldu")
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
