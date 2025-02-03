e424d445c9f2b16be4553c0673f75dea8f38463a
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-02-03 03:24:44 +0300

    error handling defined

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import express, { Request } from 'express'
 import { PrismaClient } from '@prisma/client'
+import { error } from 'console';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 const app = express()
 const port = 3000
@@ -13,20 +15,28 @@ app.get('/', (req, res) => {
 
 interface CreateUser {
     name: string;
-    email: string
+    email: string;
 }
 
 app.post("/users", async (req: Request<{}, {}, CreateUser>, res) => {
     const data = req.body;
 
-    const user = await prisma.user.create({
-        data: {
-            name: data.name,
-            email: data.email,
-        },
-    })
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: data.name,
+                email: data.email,
+            },
+        })
+        res.json(user)
 
-    res.json(user)
+    } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+            if (e.code === "P2002") {
+                res.status(409).json({ message: "Bu e-posta adresi sistemde zaten kayıtlı" })
+            }
+        }
+    }
 })
 
 app.listen(port, () => {
