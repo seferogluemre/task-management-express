0891014f8a1e89c0f649f7a36ffef820e36d57cb
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-02-03 03:03:22 +0300

    post user data

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-const express = require('express')
+import express, { Request } from 'express'
+import { PrismaClient } from '@prisma/client'
+
 const app = express()
 const port = 3000
+const prisma = new PrismaClient();
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post("/users", (req, res) => {
+app.post("/users", (req: Request<{}, {}, { name: string; email: string }>, res) => {
+    const data = req.body;
+    console.log(data)
+
+
     res.send("Kullanıcı oluşturuldu")
 })
 
